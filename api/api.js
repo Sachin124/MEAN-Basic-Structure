26d3a7de5211c70ac6afc766394ca8fd7ee1aa17
Etpl <sachin.b@exceptionaire.co>
2019-07-04 17:23:34 +0530

    mean stack with mongo db

--- api/api.js
+++ api/api.js
@@ -13,17 +13,33 @@ router.get('/', (req, res) => {
   res.send('api works');
 });
 
+const {
+  MongoClient
+} = require('mongodb');
+
 // Get all posts
 router.get('/posts', (req, res) => {
   // Get posts from the mock api
   // This should ideally be replaced with a service that connects to MongoDB
   axios.get(`${API}/posts`)
     .then(posts => {
-      res.status(200).json(posts.data);
+      // res.status(200).json(posts.data);
+
+    MongoClient.connect('mongodb://localhost:27017/mean', (err, db) => {
+    db.collection('angular').find().toArray().then((docs) => {
+        if (err) {
+            return console.log('Unable TO fetch data');
+        }
+        // console.log(JSON.stringify(docs, undefined, 2));
+        res.status(200).json(docs);
+    });
+});
+
     })
     .catch(error => {
       res.status(500).send(error)
     });
 });
+
 
 module.exports = router;
